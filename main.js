5cc887c8ce3ef9fbd87cefbaa52fcbca9bf776b2
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 09:52:08 +0300

    Functions 1

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('Hello World!');
+var x=5
+var y=5
+document. write(x==y); 
+document. write('<br>');
+var fullname='John';
+var lastname='Peace';
+fullname +=lastname;
+document. write (fullname);
+var x=5;
+x++;
+document.write(x);
+
+function writeText(str){document. getElementById("alt"). innerHTML=str} 
+writeText("Hello There");
+function addNumbers(num1,num2)
+var sum= num1 + num2;
+{document.getElementById("ray"). innerHTML=sum;} 
+addNumbers("5,6");
+var person={ fullName:'John Doe', 
+age:'40',
+showName:function(){alert(this. fullName);
+    console.log(alerted);
+} 
+showAge:function(){alert(this. age);} 
